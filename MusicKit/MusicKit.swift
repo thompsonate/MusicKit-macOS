2ce4a2519c9ea7a016a989c08abaf4ed3b45121a
thompsonate <nate@natethompson.io>
2020-02-10 03:28:02 -0500

    Create Xcode project

---
+++ MusicKit/MusicKit.swift
@@ -0,0 +1,13 @@
+//
+//  MusicKit.swift
+//  MusicKit
+//
+//  Created by Nate Thompson on 2/10/20.
+//  Copyright © 2020 Nate Thompson. All rights reserved.
+//
+
+import Foundation
+
+open class MusicKit {
+
+}
